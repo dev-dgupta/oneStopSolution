d17240fcdcb311f0174bdef953377eca9306a39b
divya.gupta <arora.gdivya2017@gmail.com>
2022-04-13 23:50:48 +0530

    shadowing concept added

--- src/main/java/ShadowingConcept.java
+++ src/main/java/ShadowingConcept.java
@@ -1,17 +1,19 @@
 public class ShadowingConcept {
 
 
-    private static final int num=10;
+    private static int num = 10;
 
 
     public static void main(String[] args) {
 
-        int num=20;
         System.out.println(num);
-
-        num=30;
+        num = 8;
+        System.out.println(num);
+        int num;
+//        System.out.println(num);
+        num = 30;
+        System.out.println(num);
         System.out.println(ShadowingConcept.num);
-        System.out.println(num);
 
 
     }
